efb9df0ae1afdaf861a871c5bd44bb0f323fbf3d
GamilMohamed <mohamed.gamil@outlook.fr>
2024-03-21 07:14:08 +0000

    push depuis instance

--- websocket.js
+++ websocket.js
@@ -3,21 +3,38 @@ const routes = {
   '/message': handleMessage,
 };
 
+const { timeStamp } = require('node:console');
+const fs = require('node:fs');
+// const content = 'Some content!';
+
 const ws = require('ws');
 const WebSocketServer = ws.Server;
 
+let clients = new Set();
+let id = 0;
 
 const wss = new WebSocketServer({ 
-  port: 8080,
+  port: 5000,
   cors: {
     origin: "*",
-    // origin: "http://localhost:5173",
     methods: ["GET", "POST"]
   }
 });
+
+function writetolog(message) {
+  const log = `[${new Date().toISOString()}] <${clients.size}> ${message}`
+  fs.appendFile('logs/logfile', log + "\n", err => {
+    if (err) {
+      console.error(err);
+    } else {
+    }
+  });
+}
+
 // WebSocket connection handling
 wss.on('connection', (ws, req) => {
-  console.log('Client connected');
+  // console.log(id, ">",req)
+  clients.add({id:id++, ws:ws})
 
   // Handle login
   ws.on('message', (data) => {
@@ -26,36 +43,58 @@ wss.on('connection', (ws, req) => {
       console.log("route is", route, "message is", message)
       const handler = routes[route];
       if (handler) {
-        handler(ws, message);
+        handler(message);
       } else {
         console.log('Unknown route:', route);
       }
+      if (message)
+        writetolog(message)
     } catch (error) {
       console.error('Error parsing message:', error);
     }
   });
 
   ws.on('close', () => {
-    broadcastMessage(ws, 'Anonyme a quitté le chat', '/logout');
+    writetolog("Someone left")
+    clients.forEach(client => {
+      if (client.ws === ws) {
+        clients.delete(client);
+        handleLogout(client.id); // Pass the id to the logout handler
+      }
+    })
   });
 });
 
 // Route handlers
-function handleLogin(message) {
-  // For anonymous users, no need to store any data
-  ws.send(JSON.stringify({ route: '/login', global: true, message: 'Anonyme a rejoint le chat'}));
+function handleLogout() {
+  broadcastGlobalMessage("/logout", "Anonyme a quitté le chat", false);
+}
+
+function handleLogin() {
+  broadcastGlobalMessage("/login", "Anonyme a rejoint", true);
 }
 
 function handleMessage(message) {
-  // Broadcast message to all clients
-  broadcastMessage(ws, message, '/message');
+  broadcastMessage('/message', `Anon: ${message}`);
+}
+
+function broadcastGlobalMessage(route, message, joined) {
+  wss.clients.forEach(user => {
+    if (user.readyState === ws.OPEN) {   
+      const mess = JSON.stringify({ route: route, global: true, message: message, joined: joined, nbUsers: clients.size});
+      console.log("sending GLOBAL", mess)
+      user.send(mess);
+    }
+  });
 }
 
 // Broadcast message to all clients
-function broadcastMessage(message, route) {
+function broadcastMessage(route, message) {
   wss.clients.forEach(client => {
-    if (client.readyState === WebSocket.OPEN) {   
-      client.send(JSON.stringify({ route: route, global: false, message: message }));
+    if (client.readyState === ws.OPEN) {   
+      const mess = JSON.stringify({ route: route, message: message, nbUsers: clients.size });
+      console.log("sending", mess)
+      client.send(mess);
     }
   });
 }
